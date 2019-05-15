e438e38963efc24cced641cb2dd95b4068b622f0
Francesco Chiusolo <chiusolofrancesco@gmail.com>
2019-05-16 00:11:12 +0200

    Remove unused property

--- Swix-Todo/Swix-Todo/Sources/AppDelegate.swift
+++ Swix-Todo/Swix-Todo/Sources/AppDelegate.swift
@@ -3,9 +3,9 @@ import Swix
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+	
 	var window: UIWindow?
-
+	
 	var store: Store<TodoState, TodoAction>!
 	var dispatcher: Dispatcher<TodoState, TodoAction>!
 	let reducers = [
@@ -13,26 +13,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		todosReducer,
 		routingReducer
 	]
-
+	
 	var router: Router?
-
+	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		window = UIWindow(frame: UIScreen.main.bounds)
-
+		
 		store = Store<TodoState, TodoAction>(state: TodoState.initial, reducers: reducers)
 		dispatcher = Dispatcher<TodoState, TodoAction>(store: store)
-
+		
 		let navigationController = UINavigationController()
-
+		
 		router = TodoRouter(navigationController: navigationController,
 							store: store,
 							dispatcher: dispatcher)
-
+		
 		router?.start()
-
+		
 		window?.rootViewController = navigationController
 		window?.makeKeyAndVisible()
 		return true
 	}
-
+	
 }
--- Swix/Swix/Sources/Dispatcher.swift
+++ Swix/Swix/Sources/Dispatcher.swift
@@ -2,7 +2,6 @@ import Foundation
 
 public class Dispatcher<State, Action> {
 
-	private var history = [Action]()
 	private let store: Store<State, Action>
 
 	public init(store: Store<State, Action>) {
@@ -10,7 +9,6 @@ public class Dispatcher<State, Action> {
 	}
 
 	public func dispatch(action: Action) {
-		history.append(action)
 		store.handle(action: action)
 	}
 
