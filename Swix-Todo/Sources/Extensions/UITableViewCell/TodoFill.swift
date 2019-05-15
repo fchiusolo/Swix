9b357fa998a8d44cd82e4839db492910ec1a13aa
Francesco Chiusolo <chiusolofrancesco@gmail.com>
2019-05-15 23:49:42 +0200

    Fix bug causing the a row to be always checked after set as such

--- Swix-Todo/Sources/Extensions/UITableViewCell/TodoFill.swift
+++ Swix-Todo/Sources/Extensions/UITableViewCell/TodoFill.swift
@@ -3,12 +3,9 @@ import UIKit
 extension UITableViewCell {
 
 	func fill(with todo: Todo) -> UITableViewCell {
-		if todo.completed {
-			accessoryType = .checkmark
-		}
-
 		textLabel?.text = todo.title
 		detailTextLabel?.text = todo.description
+		accessoryType = todo.completed ? .checkmark : .none
 		return self
 	}
 
